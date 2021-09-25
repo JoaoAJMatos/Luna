e57b765804d9cc77d59509d1457d8510ac5df35c
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2021-09-25 11:36:50 -0400

    Difficulty and Nonce Value - Proof of work

--- block.js
+++ block.js
@@ -2,11 +2,13 @@ const { GENESIS_DATA } = require('./config');
 const cryptoHash = require('./crypto-hash');
 
 class Block {
-    constructor({ timestamp, lastHash, hash, data }) {
-        this.timestamp = timestamp;
-        this.lastHash  = lastHash;
-        this.hash      = hash;
-        this.data      = data;
+    constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
+        this.timestamp  = timestamp;
+        this.lastHash   = lastHash;
+        this.hash       = hash;
+        this.data       = data;
+        this.nonce      = nonce;
+        this.difficulty = difficulty;
     }
 
     static genesis() {
@@ -14,15 +16,21 @@ class Block {
     }
 
     static mineBlock({ lastBlock, data }) {
-        const timestamp = Date.now();
-        const lastHash  = lastBlock.hash;
+        let hash, timestamp;
+        //const timestamp      = Date.now();
+        const lastHash       = lastBlock.hash;
+        const { difficulty } = lastBlock;
+        let nonce = 0;
+
+        do { // Proof of work
+
+            nonce++;
+            timestamp = Date.now();
+            hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
+
+        } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
         
-        return new this({
-            timestamp,
-            lastHash,
-            data,
-            hash: cryptoHash(timestamp, lastHash, data)
-        });
+        return new this({ timestamp, lastHash, data, difficulty, nonce, hash });
     }
 }
 
--- config.js
+++ config.js
@@ -1,8 +1,12 @@
+const INITIAL_DIFFICULTY = 3;
+
 const GENESIS_DATA = {
-    timestamp: 1632567704,
-    lastHash: '970ec274ca867815174ebe4eff19282000f9495a6c7254e94991d1fb4dc3df30',
-    hash: 'todo-hash',
-    data: []
+    timestamp  : 1632567704,
+    lastHash   : '970ec274ca867815174ebe4eff19282000f9495a6c7254e94991d1fb4dc3df30',
+    hash       : 'todo-hash',
+    difficulty : INITIAL_DIFFICULTY,
+    nonce      : 0,
+    data       : []
 };
 
 module.exports = { GENESIS_DATA };
