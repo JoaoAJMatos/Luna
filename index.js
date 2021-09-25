f5d6426f25b20fc6be66ae018f5e615146cd6f97
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2021-09-25 16:35:47 -0400

    Post request to mine a block

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const Blockchain = require('./blockchain');
 
 const app = express();
 const blockchain = new Blockchain();
 
+app.use(bodyParser.json());
+
+// Returns the blockchain data as JSON
 app.get('/api/blocks', (req, res) => {
     res.json(blockchain.chain);
+});
+
+// Mine blocks in the transaction pool
+app.post('/api/mine', (req, res) => {
+    const { data } = req.body;
+
+    blockchain.addBlock({ data });
+
+    res.redirect('/api/blocks');
 });
 
 const PORT = 3000;
