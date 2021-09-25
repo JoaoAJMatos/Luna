4ce3de968d68f39313c0199bf3b73d05e3426dfd
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2021-09-25 17:48:59 -0400

    Broadcast Chain

--- blockchain.js
+++ blockchain.js
@@ -18,16 +18,16 @@ class Blockchain {
     // Replace current chain by the incoming chain
     replaceChain(chain) {
         if (chain.length <= this.chain.length) {
-            console.error('The incoming chain must be longer');
+            console.error('[-] The incoming chain must be longer');
             return;
         }
 
         if (!Blockchain.isValidChain(chain)) {
-            console.error('The incoming chain must be valid');
+            console.error('[-] The incoming chain must be valid');
             return;
         }
 
-        console.log('Replacing chain with', chain);
+        console.log('[+] Replacing chain with', chain);
         this.chain = chain;
     }
 
--- pubsub.js
+++ pubsub.js
@@ -2,24 +2,48 @@
 const redis = require('redis');
 
 const CHANNELS = {
-    TEST: 'TEST'
+    TEST: 'TEST',
+    BLOCKCHAIN: 'BLOCKCHAIN'
 }
 
 class PubSub {
-    constructor() {
+    constructor({ blockchain }) {
+        this.blockchain = blockchain;
+
         this.publisher  = redis.createClient();
         this.subscriber = redis.createClient();
 
-        this.subscriber.subscribe(CHANNELS.TEST);
+        this.subscribeToChannels();
 
         this.subscriber.on('message', (channel, message) => this.handleMessage(channel, message));
     }
 
     handleMessage(channel, message) {
         console.log(`Message received | Channel: ${channel} | Message: ${message}`);
+
+        const parsedMessage = JSON.parse(message);
+
+        if (channel = CHANNELS.BLOCKCHAIN) { // Update chain if incoming chain is valid
+            this.blockchain.replaceChain(parsedMessage);
+        }
+    }
+
+    subscribeToChannels() { // Subscribe client to all available channels
+        Object.values(CHANNELS).forEach(channel => { 
+            this.subscriber.subscribe(channel); 
+        });
+    }
+
+    publish({ channel, message }) {
+        this.publisher.publish(channel, message);
+    }
+
+    broadcastChain() {
+        this.publish({
+            channel: CHANNELS.BLOCKCHAIN,
+            message: JSON.stringify(this.blockchain.chain)
+        });
     }
 }
 
-const testPubSub = new PubSub();
-
-setTimeout(() => testPubSub.publisher.publish(CHANNELS.TEST, 'foo'), 1000);
+module.exports = PubSub;
