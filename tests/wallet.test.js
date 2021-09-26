3e0f13b91e99baba24f990b4d4611b52d9d5f71c
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2021-09-26 13:09:12 -0400

    Wallet Create Transaction

--- tests/wallet.test.js
+++ tests/wallet.test.js
@@ -1,4 +1,5 @@
 const Wallet = require('../wallet');
+const Transaction = require('../wallet/transaction');
 const { verifySignature } = require('../util');
 
 describe('Wallet', () => {
@@ -39,6 +40,37 @@ describe('Wallet', () => {
                     signature: new Wallet().sign(data)
                 })
             ).toBe(false);
+        });
+    });
+
+    describe('createTransaction()', () => {
+        describe('and the amount exceeds the balance', () => {
+            it('throws an error', () => {
+                expect(() => wallet.createTransaction({ amount: 999999, recipient: 'test-recipient' }))
+                                                                    .toThrow('Amount exceeds balance');
+            });
+        });
+
+        describe('and the amount is valid', () => {
+            let transaction, amount, recipient;
+
+            beforeEach(() => { // Create new transaction for every test
+                amount = 5;
+                recipient = 'test-recipient';
+                transaction = wallet.createTransaction({ amount, recipient });
+            });
+
+            it('creates an instance of `Transaction`', () => {
+                expect(transaction instanceof Transaction).toBe(true);
+            });
+
+            it('matches the transaction input with the wallet', () => {
+                expect(transaction.input.address).toEqual(wallet.publicKey);
+            });
+
+            it('outputs the amount to the recipient', () => {
+                expect(transaction.outputMap[recipient]).toEqual(amount);
+            });
         });
     });
 });
