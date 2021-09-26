5a8b64833f731de85525f41b0e89d686106e4838
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2021-09-26 12:39:48 -0400

    Validate Transaction

--- tests/transaction.test.js
+++ tests/transaction.test.js
@@ -57,4 +57,40 @@ describe('Transaction', () => {
             );
         });
     });
+
+    describe('validTransaction()', () => {
+        let errorMock;
+
+        beforeEach(() => {
+            errorMock = jest.fn();
+        
+            global.console.error = errorMock;
+        });
+
+        describe('when the transaction is valid', () => {
+            it('returns true', () => {
+                expect(Transaction.validTransaction(transaction)).toBe(true);
+            });
+        });
+
+        describe('when the transaction is invalid', () => {
+            describe('and a transaction outputMap valie is invalid', () => {
+                it('returns false and logs an error', () => {
+                    transaction.outputMap[senderWallet.publicKey] = 999999;
+
+                    expect(Transaction.validTransaction(transaction)).toBe(false);
+                    expect(errorMock).toHaveBeenCalled();
+                });
+            });
+
+            describe('and the transaction input signature is invalid', () => {
+                it('returns false and logs an error', () => {
+                    transaction.input.signature = new Wallet().sign('data');
+
+                    expect(Transaction.validTransaction(transaction)).toBe(false);
+                    expect(errorMock).toHaveBeenCalled();
+                });
+            });
+        });
+    });
 });
