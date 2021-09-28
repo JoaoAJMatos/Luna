a02052915638b6e5bda1df3f874000d7179dff11
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2021-09-28 14:32:55 -0400

    Solved re-signing bug

--- tests/crypto-hash.test.js
+++ tests/crypto-hash.test.js
@@ -3,10 +3,18 @@ const { cryptoHash } = require('../util');
 describe('cryptoHash()', () => {
 
     it('generates a SHA-256 hashed output', () => {
-        expect(cryptoHash('luna')).toEqual('970ec274ca867815174ebe4eff19282000f9495a6c7254e94991d1fb4dc3df30')
+        expect(cryptoHash('luna')).toEqual('74a8e5bfb10432d38c26809d53222d39434a41c99e56ea4dd68614ff81b709af')
     });
 
     it('produces the same hash with the same input arguments in any order', () => {
         expect(cryptoHash('one', 'two', 'three')).toEqual(cryptoHash('three', 'one', 'two'));
+    });
+
+    it('produces a unique hash when the properties have changed on an input', () => {
+        const obj = {};
+        const originalHash = cryptoHash(obj);
+        obj['a'] = 'a';
+
+        expect(cryptoHash(obj)).not.toEqual(originalHash);
     });
 });
--- util/crypto-hash.js
+++ util/crypto-hash.js
@@ -1,9 +1,10 @@
 const crypto = require('crypto');
 
+// Returns the SHA-256 hash of a map of inputs
 const cryptoHash = (...inputs) => {
     const hash = crypto.createHash('sha256');
     
-    hash.update(inputs.sort().join(' '));
+    hash.update(inputs.map(input => JSON.stringify(input)).sort().join(' '));
 
     return hash.digest('hex');
 };
