1f6d5b3420eaa22eba51eeceeb2b55e3577d6985
Foxy <joaoafonsoj2004@gmail.com>
2021-10-09 08:44:54 -0400

    Transaction Updates in the API

--- index.js
+++ index.js
@@ -36,10 +36,14 @@ app.post('/api/mine', (req, res) => {
 app.post('/api/transact', (req, res) => {
     const { amount, recipient } = req.body;
 
-    let transaction;
-
+    let transaction = transactionPool.existingTransaction({ inputAddress: wallet.publicKey });
+ 
     try {
-        transaction = wallet.createTransaction({ recipient, amount });
+        if (transaction) {
+            transaction.update({ senderWallet: wallet, recipient, amount });
+        } else {
+            transaction = wallet.createTransaction({ recipient, amount });
+        }
     }  catch(error) {
         return res.status(400).json({ type: 'error', message: error.message });
     } 
--- tests/transaction-pool.test.js
+++ tests/transaction-pool.test.js
@@ -3,11 +3,12 @@ const Transaction     = require('../wallet/transaction');
 const Wallet          = require('../wallet/index');
 
 describe('TransactionPool', () => {
-    let transactionPool, transaction;
+    let transactionPool, transaction, senderWallet;
 
     beforeEach(() => {
         transactionPool = new TransactionPool();
-        transaction     = new Transaction({ senderWallet: new Wallet(), recipient: 'test', amount: 2 });
+        senderWallet    = new Wallet()
+        transaction     = new Transaction({ senderWallet, recipient: 'test', amount: 2 });
     });
 
     describe('setTransaction()', () => {
@@ -15,6 +16,16 @@ describe('TransactionPool', () => {
             transactionPool.setTransaction(transaction);
         
             expect(transactionPool.transactionMap[transaction.id]).toBe(transaction);
+        });
+    });
+
+    describe('existingTransaction()', () => {
+        it('returns an existing transaction given an input address', () => {
+            transactionPool.setTransaction(transaction);
+
+            expect(
+                transactionPool.existingTransaction({ inputAddress: senderWallet.publicKey })
+            ).toBe(transaction);
         });
     });
 });
