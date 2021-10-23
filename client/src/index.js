2c9319a8f9711a4c0dd255b853488af66b634bb3
Foxy <joaoafonsoj2004@gmail.com>
2021-10-23 13:36:35 -0400

    App Component

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/App';
 
 render(
-    <div>Welcome to Luna... Live 3</div>, 
+    <App/>, 
     document.getElementById('root')
 );
