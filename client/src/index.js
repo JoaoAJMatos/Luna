3bcd8004c5e1940582f3f2251e101520c15b5629
Foxy <joaoafonsoj2004@gmail.com>
2021-10-23 13:27:41 -0400

    Frontend Development workflow

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 
 render(
-    <div>Welcome to Luna...</div>, 
+    <div>Welcome to Luna... Live 3</div>, 
     document.getElementById('root')
 );
