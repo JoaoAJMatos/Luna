42d125effaf6a3fe081060c281a570552eac6b5a
Foxy <joaoafonsoj2004@gmail.com>
2021-11-09 13:30:07 -0500

    Updated dedicated-miner

--- dedicated-miner.py
+++ dedicated-miner.py
@@ -4,9 +4,12 @@ import time
 import sys
 import threading
 import itertools
+import os
 
 transactionPool_URL = "http://localhost:3000/api/transaction-pool-map"
 mineTransactions_URL = "http://localhost:3000/api/mine-transactions"
+
+done = False
 
 def loadingAnim():
           for c in itertools.cycle(['|', '/', '-', '\\']):
@@ -17,17 +20,35 @@ def loadingAnim():
                     time.sleep(0.1)
           sys.stdout.write('\r[+] Done!    ')
 
-while True:
-          transactionPool = requests.get(url = transactionPool_URL).json()
+def fetchMine():
+          global done
 
-          if (len(transactionPool) > 0):
-                    print('[MESSAGE] Transaction-Pool has been updated!')
-                    mining_start = time.time()
-                    done = False
-                    t = threading.Thread(target=loadingAnim)
-                    t.start()
-                    mining = requests.get(url = mineTransactions_URL)
-                    done = True
-                    t.join()
-                    mining_end = time.time()
-                    print("\n[+] Successfuly mined block. Time elapsed: " + str(mining_end - mining_start) + "ms")
+          while True:
+
+                    transactionPool = requests.get(url = transactionPool_URL)
+                    poolSize        = len(transactionPool.json())
+                    
+                    if transactionPool.status_code == 200:
+
+                              if (poolSize > 0):
+                                        print(f'[MSG] Transaction-Pool has been updated! (size: {poolSize})')
+                                        mining_start = time.time()
+                                        # Threading for mining animation
+                                        t = threading.Thread(target=loadingAnim)
+                                        t.start()
+                                        # Send mine request and stop thread 
+                                        mining = requests.get(url = mineTransactions_URL)
+                                        done = True
+                                                  
+                                        t.join()
+                                        mining_end = time.time()
+                                        if (mining.status_code == 200):
+                                                  print("\n[+] Successfuly mined block. Time elapsed: " + str(mining_end - mining_start)[:-12] + " seconds")
+                    
+
+def main():
+          print('[*] Waiting for new transactions')
+          fetchMine()
+
+if __name__ == '__main__':
+          main()
